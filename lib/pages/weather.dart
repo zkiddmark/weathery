ffba3a8148e0d5c580e984890eea9ad6b86b4f4a
Peter Stjern <peter.stjern@p-bolaget.goteborg.se>
2021-07-12 15:31:36 +0200

    feat fixed searchField, began implementation of wind direction.

--- lib/pages/weather.dart
+++ lib/pages/weather.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weathery/services/weather-service.dart';
+import 'dart:math' as math;
 
 class Weather extends StatefulWidget {
   const Weather({Key? key}) : super(key: key);
@@ -13,15 +14,18 @@ class _WeatherState extends State<Weather> {
   late Map<String, WeatherService> weatherService =
       ModalRoute.of(context)!.settings.arguments as Map<String, WeatherService>;
 
+  TextEditingController _textEditingController = TextEditingController();
   void updateWeatherLocation(String location) async {
     var ws = weatherService['data'] as WeatherService;
     await ws.getWeatherByLocation(location);
     setState(() => {weather = ws.currentWeather});
   }
 
-  Widget createWeatherCard(WeatherResponse weather) {
-    var iconUrl =
-        'http://openweathermap.org/img/wn/${weather.weather.first.icon}@2x.png';
+  _createWeatherCard(WeatherResponse weather) {
+    // ignore: unnecessary_null_comparison
+    var iconUrl = weather.weather.first.icon != null
+        ? 'http://openweathermap.org/img/wn/${weather.weather.first.icon}@2x.png'
+        : 'local';
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Card(
@@ -31,53 +35,90 @@ class _WeatherState extends State<Weather> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.network(
-                  iconUrl,
-                ),
+                iconUrl == 'local'
+                    ? Icon(Icons.circle)
+                    : Image.network(
+                        iconUrl,
+                      ),
               ],
             ),
             Divider(
               color: Colors.black26,
             ),
             Text('The temperature is ${weather.temp}'),
-            Text(weather.weather.first.description)
+            Text(weather.weather.first.description),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _searchField(BuildContext context, Function onSubmit) {
-    var textFieldController = TextEditingController();
+  _searchField(BuildContext context) {
     return Container(
-      height: 300,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Container(
-              decoration: BoxDecoration(color: Colors.white30),
-              child: TextField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.search),
-                  labelText: 'Enter a city name',
-                  focusColor: Colors.white70,
-                  focusedBorder: InputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.amber,
-                    ),
-                  ),
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+          child: TextField(
+            decoration: InputDecoration(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              labelText: 'Enter a city name',
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
                 ),
-                cursorColor: Colors.red,
-                controller: textFieldController,
-                onEditingComplete: () => {
-                  updateWeatherLocation(textFieldController.text),
-                },
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+              labelStyle: TextStyle(
+                color: Colors.white,
               ),
             ),
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            cursorColor: Colors.red,
+            controller: _textEditingController,
+            onEditingComplete: () => {
+              updateWeatherLocation(_textEditingController.text),
+            },
           ),
+        ),
+      ),
+    );
+  }
+
+  _windDirection(double angle) {
+    return Transform.rotate(
+      angle: angle * math.pi / 180,
+      child: Icon(Icons.arrow_upward),
+    );
+  }
+
+  _body() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            'https://images.unsplash.com/photo-1581150257735-1c93ea8306ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          ),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _searchField(context),
+          _createWeatherCard(weather),
+          _windDirection(293),
         ],
       ),
     );
@@ -87,31 +128,37 @@ class _WeatherState extends State<Weather> {
   Widget build(BuildContext context) {
     var ws = weatherService['data'] as WeatherService;
     weather = ws.currentWeather;
-    // print(weather.name);
-    return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Weather for ${weather.name}'),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(
-                'https://images.unsplash.com/photo-1581150257735-1c93ea8306ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    return GestureDetector(
+      // This is to enable the keyboard to disappear when tapping outside of it.
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Weather for ${weather.name}'),
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                  'https://images.unsplash.com/photo-1581150257735-1c93ea8306ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+                ),
               ),
             ),
-          ),
-          child: Column(
-            children: [
-              Center(
-                child: _searchField(context, ws.updateLocation),
-              ),
-              Center(
-                child: createWeatherCard(weather),
-              ),
-            ],
-          ),
-        ));
+            child: _body(),
+          )),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
   }
 }
